0c2fe988a2478150ab4e492434f7e0d2d14fcf4b
ji-no <jino.jpn@gmail.com>
2023-06-08 23:33:35 +0900

    Initial iOS App

---
+++ NewRelicCrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by ji-no on 2023/06/08
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
