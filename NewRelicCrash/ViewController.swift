dabd8b95ef8eef4b8aac2dc1b3f439807ae13432
ji-no <jino.jpn@gmail.com>
2023-06-08 23:35:02 +0900

    add WebView

--- NewRelicCrash/ViewController.swift
+++ NewRelicCrash/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    private lazy var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = WKWebsiteDataStore.default()
+        return WKWebView(frame: .zero, configuration: configuration)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+        let url = URL(string: "https://newrelic.com/")!
+        let req = URLRequest(url: url)
+        self.webView.load(req)
     }
 
 
